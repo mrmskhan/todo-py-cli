6773e97d549beebf769c1baad0ae29ff6f26f524
M. SALEEM KHAN <160552274+mrmskhan@users.noreply.github.com>
2025-04-04 07:05:27 +0500

    remove comment

--- todo.py
+++ todo.py
@@ -5,7 +5,7 @@ import os     # to check if the file exists
 TODO_FILE = "todo.json"
 
 def load_tasks():
-    if not os.path.exists(TODO_FILE):  # اگر فائل موجود نہ ہو
+    if not os.path.exists(TODO_FILE):  
         return []
     with open(TODO_FILE, "r") as file:
         return json.load(file)
@@ -24,7 +24,7 @@ def cli():
 def add(task):         
     """Add a new task to the list"""
     tasks = load_tasks()
-    tasks.append({"task": task, "done": False})  # یہاں درست کیا
+    tasks.append({"task": task, "done": False}) 
     save_tasks(tasks)
     click.echo(f"Task added successfully: {task}")
 
